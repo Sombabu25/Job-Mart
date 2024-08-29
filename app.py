d71e700b72dc3fa5ffcc0e10eb69d0b89e77c57b
Sombabu Patel <133793786+Sombabu25@users.noreply.github.com>
2024-08-29 17:46:42 +0000

    bootstrap and css added

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 
-from flask import Flask
+from flask import Flask,render_template
 app=Flask(__name__)
 
 @app.route("/")
 def greet():
-  return "Hello World to fg me"
+  return render_template("home.html")
 
 
 
